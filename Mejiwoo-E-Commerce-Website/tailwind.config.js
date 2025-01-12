2226891782bdec2335b22c44ba98feb129fbbac0
Kshitija <kshitijawork012@gmail.com>
2025-01-12 16:56:48 +0530

    Add files via upload

---
+++ Mejiwoo-E-Commerce-Website/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+      extend: {
+        animation: {
+          marquee: 'marquee 20s linear infinite',
+        },
+        keyframes: {
+          marquee: {
+            '0%': {
+              transform: 'translateX(0)',
+            },
+            '100%': {
+              transform: 'translateX(-100%)',
+            },
+          },
+        },
+      },
+  },
+  plugins: [],
+}
+
+
+
